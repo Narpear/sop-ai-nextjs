66463131c8d150cc7c1379fa0f3f85ca8c5444c5
Narpear <prer.kulk@gmail.com>
2025-03-30 22:59:58 +0530

    made route export field valied

---
+++ src/app/api/auth/[...nextauth]/auth-options.ts
@@ -0,0 +1,38 @@
+import CredentialsProvider from "next-auth/providers/credentials";
+import bcrypt from "bcryptjs";
+import { connectMongoDB } from "@/app/lib/mongodb";
+import User from "@/models/user";
+
+export const authOptions = {
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "text", placeholder: "you@example.com" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials?.password) {
+          throw new Error("Missing email or password");
+        }
+
+        await connectMongoDB();
+        const user = await User.findOne({ email: credentials.email });
+
+        if (!user) {
+          throw new Error("User not found");
+        }
+
+        const passwordsMatch = await bcrypt.compare(credentials.password, user.password);
+        if (!passwordsMatch) {
+          throw new Error("Incorrect password");
+        }
+
+        return { id: user._id, email: user.email, name: user.fname };
+      },
+    }),
+  ],
+  session: { strategy: "jwt" },
+  secret: process.env.NEXTAUTH_SECRET,
+  pages: { signIn: "/" },
+};
